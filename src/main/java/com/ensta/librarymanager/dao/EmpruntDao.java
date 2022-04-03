e1a377721dcddc577da079da9636a20394f561b2
liam-kelley <liam.kelley.144@gmail.com>
2022-04-04 00:52:40 +0200

    50% EmpruntDao.java

--- src/main/java/com/ensta/librarymanager/dao/EmpruntDao.java
+++ src/main/java/com/ensta/librarymanager/dao/EmpruntDao.java
@@ -54,20 +54,70 @@ public class EmpruntDao implements IEmpruntDao {
 
     @Override
     public List<Emprunt> getListCurrent() throws DaoException {
-        // TODO Auto-generated method stub
-        return null;
+        try (Connection conn = ConnectionManager.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("SELECT e.id AS id, idMembre, nom, prenom, adresse, email, telephone, abonnement, idLivre, titre, auteur, isbn, dateEmprunt, dateRetour FROM emprunt AS e INNER JOIN membre ON membre.id = e.idMembre INNER JOIN livre ON livre.id = e.idLivre WHERE dateRetour IS NULL"))
+        {
+            List<Emprunt> liste = new ArrayList<>();
+            ResultSet res = stmt.executeQuery();
+
+            while (res.next()) { //les dates de retour seront toutes null
+                liste.add(new Emprunt(res.getInt("id"), res.getInt("idMembre"), res.getInt("idLivre"), res.getDate("dateEmprunt").toLocalDate(), null));
+            }
+            return liste;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new DaoException();
+        }
     }
 
     @Override
     public List<Emprunt> getListCurrentByMembre(int idMembre) throws DaoException {
-        // TODO Auto-generated method stub
-        return null;
+        try (Connection conn = ConnectionManager.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("SELECT e.id AS id, idMembre, nom, prenom, adresse, email, telephone, abonnement, idLivre, titre, auteur, isbn, dateEmprunt, dateRetour FROM emprunt AS e INNER JOIN membre ON membre.id = e.idMembre INNER JOIN livre ON livre.id = e.idLivre WHERE dateRetour IS NULL AND membre.id = ?"))
+        {
+            stmt.setInt(1, idMembre); //Gestion du ?
+
+            List<Emprunt> liste = new ArrayList<>();
+            
+            ResultSet res = stmt.executeQuery();
+
+            while (res.next()) {
+                LocalDate dateRetour;
+                if (res.getDate("dateRetour") != null) { dateRetour = res.getDate("dateRetour").toLocalDate();}
+                else {dateRetour = null;}
+                
+                liste.add(new Emprunt(res.getInt("id"), res.getInt("idMembre"), res.getInt("idLivre"), res.getDate("dateEmprunt").toLocalDate(), dateRetour));
+            }
+            return liste;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new DaoException();
+        }
     }
 
     @Override
     public List<Emprunt> getListCurrentByLivre(int idLivre) throws DaoException {
-        // TODO Auto-generated method stub
-        return null;
+        try (Connection conn = ConnectionManager.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("SELECT e.id AS id, idMembre, nom, prenom, adresse, email, telephone, abonnement, idLivre, titre, auteur, isbn, dateEmprunt, dateRetour FROM emprunt AS e INNER JOIN membre ON membre.id = e.idMembre INNER JOIN livre ON livre.id = e.idLivre WHERE dateRetour IS NULL AND livre.id = ?"))
+        {
+            stmt.setInt(1, idLivre); //Gestion du ?
+
+            List<Emprunt> liste = new ArrayList<>();
+            
+            ResultSet res = stmt.executeQuery();
+
+            while (res.next()) {
+                LocalDate dateRetour;
+                if (res.getDate("dateRetour") != null) { dateRetour = res.getDate("dateRetour").toLocalDate();}
+                else {dateRetour = null;}
+                
+                liste.add(new Emprunt(res.getInt("id"), res.getInt("idMembre"), res.getInt("idLivre"), res.getDate("dateEmprunt").toLocalDate(), dateRetour));
+            }
+            return liste;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new DaoException();
+        }
     }
 
     @Override
