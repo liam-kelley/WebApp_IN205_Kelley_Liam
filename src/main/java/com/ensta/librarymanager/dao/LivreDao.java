2d38802c6b670609e31cce36ba5fd6f8603d716e
liam-kelley <liam.kelley.144@gmail.com>
2022-04-03 23:14:58 +0200

    LivreDAO.getList(), premieres requetes SQL

--- src/main/java/com/ensta/librarymanager/dao/LivreDao.java
+++ src/main/java/com/ensta/librarymanager/dao/LivreDao.java
@@ -1,17 +1,48 @@
 package com.ensta.librarymanager.dao;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import com.ensta.librarymanager.dao.daoInterfaces.ILivreDao;
 import com.ensta.librarymanager.exception.DaoException;
 import com.ensta.librarymanager.modele.Livre;
+import com.ensta.librarymanager.persistence.ConnectionManager;
 
 public class LivreDao implements ILivreDao{
 
+    private static LivreDao instance;
+    private LivreDao() {}
+    public static LivreDao getInstance() {
+        if (instance == null) {
+            instance = new LivreDao();
+        }
+        return instance;
+    }
+
     @Override
     public List<Livre> getList() throws DaoException {
-        // TODO Auto-generated method stub
-        return null;
+        try(Connection conn = ConnectionManager.getConnection()){
+
+            List<Livre> listLivres = new ArrayList<>();
+
+            PreparedStatement pstmt = conn.prepareStatement("SELECT id, titre, auteur, ISBN FROM livre");
+            ResultSet rs = pstmt.executeQuery();
+
+            while(rs.next()){
+                listLivres.add(new Livre(rs.getInt("id"), rs.getString("titre"), rs.getString("auteur"), rs.getString("ISBN")));
+            }
+            return listLivres;
+            
+        } catch(SQLException e){
+            e.printStackTrace();
+            throw new DaoException();
+        }
     }
 
     @Override
