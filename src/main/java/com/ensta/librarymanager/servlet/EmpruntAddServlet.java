4868a703cd1768b229e7de4eedb4f9eb2128317c
liam-kelley <liam.kelley.144@gmail.com>
2022-04-04 04:01:06 +0200

    EmpruntAddServlet.doGet() ??

--- src/main/java/com/ensta/librarymanager/servlet/EmpruntAddServlet.java
+++ src/main/java/com/ensta/librarymanager/servlet/EmpruntAddServlet.java
@@ -24,11 +24,24 @@ public class EmpruntAddServlet extends HttpServlet {
     MembreService membreService = MembreService.getInstance();
     EmpruntService empruntService = EmpruntService.getInstance();
 
+    //La méthode doGet() permet d’afficher un formulaire d’ajout
+    // d’emprunt, basé sur deux champs de type <select> qui ne devront
+    // contenir respectivement que les livres disponibles à l’emprunt et
+    // que les membres pouvant réaliser un nouvel emprunt.
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        try {
+            request.setAttribute("listLivreDispo", livreService.getListDispo());
+            request.setAttribute("listMembreEP", membreService.getListMembreEmpruntPossible());
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
     }
 
+    // La méthode doPost() a pour unique rôle de traiter le formulaire de
+    // création d’un nouvel emprunt à partir des données récupérées via
+    // le formulaire précédent.
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
