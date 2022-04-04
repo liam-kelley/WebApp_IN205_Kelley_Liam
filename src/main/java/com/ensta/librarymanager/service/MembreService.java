b8be65fe6211c92a773af6178179a38d41ece9ba
liam-kelley <liam.kelley.144@gmail.com>
2022-04-04 02:47:18 +0200

    MembreService.java, fin de services et l'exo 4!

--- src/main/java/com/ensta/librarymanager/service/MembreService.java
+++ src/main/java/com/ensta/librarymanager/service/MembreService.java
@@ -1,8 +1,10 @@
 package com.ensta.librarymanager.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.ensta.librarymanager.dao.MembreDao;
+import com.ensta.librarymanager.exception.DaoException;
 import com.ensta.librarymanager.exception.ServiceException;
 import com.ensta.librarymanager.modele.Membre;
 import com.ensta.librarymanager.service.servicesInterfaces.IMembreService;
@@ -24,45 +26,85 @@ public class MembreService implements IMembreService{
 
     @Override
     public List<Membre> getList() throws ServiceException {
-        // TODO Auto-generated method stub
-        return null;
+        try {
+            return membreDao.getList();
+        } catch (DaoException e) {
+            e.printStackTrace();
+            throw new ServiceException();
+        }
     }
 
     @Override
     public List<Membre> getListMembreEmpruntPossible() throws ServiceException {
-        // TODO Auto-generated method stub
-        return null;
+        try {
+            List<Membre> listeMembres = membreDao.getList();
+            List<Membre> listeMembreEP = new ArrayList<>();
+            for (Membre membre : listeMembres) {
+                if (empruntService.isEmpruntPossible(membre)) { listeMembreEP.add(membre); }
+            }
+            return listeMembreEP;
+        } catch (DaoException e) {
+            e.printStackTrace();
+            throw new ServiceException();
+        }
     }
 
     @Override
     public Membre getById(int id) throws ServiceException {
-        // TODO Auto-generated method stub
-        return null;
+        try {
+            if (membreDao.getById(id).isPresent()) {
+                Membre ref = membreDao.getById(id).get();
+                return ref;
+            }
+            else {System.out.println("Le membre n'est pas dans la base de donnée");}
+            return null;
+        } catch (DaoException e) {
+            e.printStackTrace();
+            throw new ServiceException();
+        }
     }
 
     @Override
-    public int create(String nom, String prenom, String adresse, String email, String telephone)
-            throws ServiceException {
-        // TODO Auto-generated method stub
-        return 0;
+    public int create(String nom, String prenom, String adresse, String email, String telephone) throws ServiceException {
+        try {
+            return membreDao.create(nom, prenom, adresse, email, telephone);
+        } catch (DaoException e) {
+            e.printStackTrace();
+            throw new ServiceException();
+        }
     }
 
     @Override
     public void update(Membre membre) throws ServiceException {
-        // TODO Auto-generated method stub
-        
+        try {
+            membreDao.update(membre);
+        } catch (DaoException e) {
+            e.printStackTrace();
+            throw new ServiceException();
+        }
     }
 
     @Override
     public void delete(int id) throws ServiceException {
-        // TODO Auto-generated method stub
-        
+        try {
+            if (membreDao.getById(id).isPresent()) {
+                membreDao.delete(membreDao.getById(id).get());
+            } else {
+                System.out.println("Le membre n'est pas dans la base de donnée");
+            }
+        } catch (DaoException e) {
+            e.printStackTrace();
+            throw new ServiceException();
+        }
     }
 
     @Override
     public int count() throws ServiceException {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-    
+        try {
+            return membreDao.count();
+        } catch (DaoException e) {
+            e.printStackTrace();
+            throw new ServiceException();
+        }
+    } 
 }
