cb4650bf9007e735b8a58eae9378a75eddce405d
liam-kelley <liam.kelley.144@gmail.com>
2022-04-04 04:24:57 +0200

    DoGet() DoPost() et Import pour tousgit add --all!

--- src/main/java/com/ensta/librarymanager/servlet/MembreListServlet.java
+++ src/main/java/com/ensta/librarymanager/servlet/MembreListServlet.java
@@ -1,8 +1,37 @@
 package com.ensta.librarymanager.servlet;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ensta.librarymanager.exception.ServiceException;
+import com.ensta.librarymanager.modele.Emprunt;
+import com.ensta.librarymanager.modele.Livre;
+import com.ensta.librarymanager.modele.Membre;
+import com.ensta.librarymanager.service.EmpruntService;
+import com.ensta.librarymanager.service.LivreService;
+import com.ensta.librarymanager.service.MembreService;
 
 @WebServlet("/membre_list")
 public class MembreListServlet extends HttpServlet {
+    LivreService livreService = LivreService.getInstance();
+    MembreService membreService = MembreService.getInstance();
+    EmpruntService empruntService = EmpruntService.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
 }
